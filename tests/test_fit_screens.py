2bfec5bd14fe66196aa01bd03f3a4fadbd243bb5
chiara.salvoni <chiara.salvoni@cgi.com>
2022-01-21 18:01:48 +0100

    ast-288 add tests and dependencies

---
+++ tests/test_fit_screens.py
@@ -0,0 +1,15 @@
+# from ska_sdp_screen_fitting.make_aterms_images import main
+""" Test screen functionality """
+
+def test_fit_kl_screens():
+    """
+    Tests kl screens generation
+    """
+    assert True
+
+
+def test_fit_voronoi_screens():
+    """
+    Tests Voronoi screens generation
+    """
+    assert True
