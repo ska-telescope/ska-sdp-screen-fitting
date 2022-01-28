38abff7536770ac24a79ae57371361e07988cd40
chiara.salvoni <chiara.salvoni@cgi.com>
2022-01-28 18:03:38 +0100

    ast-288 test module instead of executable

--- tests/test_fit_screens.py
+++ tests/test_fit_screens.py
@@ -3,25 +3,14 @@
 import os
 import shutil
 import uuid
-from subprocess import check_call
 
 import pytest
+
+from ska_sdp_screen_fitting.make_aterm_images import make_aterm_image
 
 CWD = os.getcwd()
 SOLFILE = "solutions.h5"
 SKYMODEL = "skymodel.txt"
-SCREEN_FITTING = "ska-sdp-screen-fitting"
-
-COMMON_ARGS = [
-    "--bounds_deg=[126.966898;63.566717;124.546030;64.608827]",
-    "--bounds_mid_deg=[125.779167;64.092778]",
-    f"--skymodel={SKYMODEL}",
-    "--solsetname=sol000",
-    "--padding_fraction=1.4",
-    "--cellsize_deg=0.2",
-    "--smooth_deg=0.1",
-    "--ncpu=0",
-]
 
 
 @pytest.fixture(autouse=True)
@@ -48,21 +37,25 @@ def test_fit_voronoi_screens():
     Tests Voronoi screens generation
     """
 
-    outroot = "tessellated"
-    check_call(
-        [
-            SCREEN_FITTING,
-            SOLFILE,
-            "--soltabname=phase000",
-            f"--screen_type={outroot}",
-            f"--outroot={outroot}",
-        ]
-        + COMMON_ARGS
+    method = "tessellated"
+    make_aterm_image(
+        SOLFILE,
+        soltabname="phase000",
+        screen_type=method,
+        outroot=method,
+        bounds_deg=[126.966898, 63.566717, 124.546030, 64.608827],
+        bounds_mid_deg=[125.779167, 64.092778],
+        skymodel=SKYMODEL,
+        solsetname="sol000",
+        padding_fraction=1.4,
+        cellsize_deg=0.2,
+        smooth_deg=0,
+        ncpu=0,
     )
 
-    assert os.path.isfile(f"{outroot}_0.fits")
-    assert os.path.isfile(f"{outroot}_template.fits")
-    assert os.path.isfile(f"{outroot}.txt")
+    assert os.path.isfile(f"{method}_0.fits")
+    assert os.path.isfile(f"{method}_template.fits")
+    assert os.path.isfile(f"{method}.txt")
 
 
 def test_fit_kl_screens():
@@ -70,17 +63,21 @@ def test_fit_kl_screens():
     Tests kl screens generation
     """
 
-    outroot = "kl"
-    check_call(
-        [
-            SCREEN_FITTING,
-            SOLFILE,
-            "--soltabname=gain000",
-            f"--screen_type={outroot}",
-            f"--outroot={outroot}",
-        ]
-        + COMMON_ARGS
+    method = "kl"
+    make_aterm_image(
+        SOLFILE,
+        soltabname="gain000",
+        screen_type=method,
+        outroot=method,
+        bounds_deg=[126.966898, 63.566717, 124.546030, 64.608827],
+        bounds_mid_deg=[125.779167, 64.092778],
+        skymodel=SKYMODEL,
+        solsetname="sol000",
+        padding_fraction=1.4,
+        cellsize_deg=0.2,
+        smooth_deg=0,
+        ncpu=0,
     )
 
-    assert os.path.isfile(f"{outroot}_0.fits")
-    assert os.path.isfile(f"{outroot}.txt")
+    assert os.path.isfile(f"{method}_0.fits")
+    assert os.path.isfile(f"{method}.txt")
