514a46a02d8bbad914ecc79c6c85bc19c76f823e
chiara.salvoni <chiara.salvoni@cgi.com>
2022-02-02 10:10:47 +0100

    ast-797 add initial (dummy) version of screen fitting

---
+++ src/ska_sdp_screen_fitting/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""
+Script to invoke creen fitting algorithm
+"""
+import argparse
+
+from ska_sdp_screen_fitting.make_aterm_images import make_aterm_image
+
+
+def start():
+    """
+    This is the entry point for the executable
+    """
+
+    description_text = "Make a-term images from solutions.\n"
+
+    parser = argparse.ArgumentParser(
+        description=description_text,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("h5parmfile", help="Filename of input H5parm")
+    parser.add_argument(
+        "--soltabname", help="Name of soltab", type=str, default="phase000"
+    )
+    parser.add_argument(
+        "--screen_type",
+        help="Type of screen",
+        type=str,
+        default="tessellated",
+    )
+    parser.add_argument(
+        "--outroot", help="Root of output images", type=str, default=""
+    )
+    parser.add_argument(
+        "--bounds_deg", help="Bounds list in deg", type=str, default=None
+    )
+    parser.add_argument(
+        "--bounds_mid_deg",
+        help="Bounds mid list in deg",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--skymodel", help="Filename of sky model", type=str, default=None
+    )
+    parser.add_argument(
+        "--solsetname", help="Solset name", type=str, default="sol000"
+    )
+    parser.add_argument(
+        "--padding_fraction", help="Padding fraction", type=float, default=1.4
+    )
+    parser.add_argument(
+        "--cellsize_deg", help="Cell size in deg", type=float, default=0.2
+    )
+    parser.add_argument(
+        "--smooth_deg", help="Smooth scale in degree", type=float, default=0.0
+    )
+    parser.add_argument(
+        "--ncpu", help="Number of CPUs to use", type=int, default=0
+    )
+    args = parser.parse_args()
+    make_aterm_image(
+        args.h5parmfile,
+        soltabname=args.soltabname,
+        screen_type=args.screen_type,
+        outroot=args.outroot,
+        bounds_deg=args.bounds_deg,
+        bounds_mid_deg=args.bounds_mid_deg,
+        skymodel=args.skymodel,
+        solsetname=args.solsetname,
+        padding_fraction=args.padding_fraction,
+        cellsize_deg=args.cellsize_deg,
+        smooth_deg=args.smooth_deg,
+        ncpu=args.ncpu,
+    )
---
+++ src/ska_sdp_screen_fitting/make_aterm_images.py
@@ -0,0 +1,22 @@
+"""
+Script to make a-term images from solutions
+"""
+
+
+def make_aterm_image(
+    h5parmfile,
+    soltabname="phase000",
+    screen_type="tessellated",
+    outroot="",
+    bounds_deg=None,
+    bounds_mid_deg=None,
+    skymodel=None,
+    solsetname="sol000",
+    padding_fraction=1.4,
+    cellsize_deg=0.2,
+    smooth_deg=0,
+    ncpu=0,
+):
+    """
+    TODO: add implementation
+    """
